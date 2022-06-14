5ebe798a1653cade1b2b51dbeff0b466fc62a142
Tarryn-M20 <102674265+Tarryn-M20@users.noreply.github.com>
2022-06-14 15:37:31 +0200

    carousel

--- NetSwag/NetSwag.Android/MainActivity.cs
+++ NetSwag/NetSwag.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace NetSwag.Droid
 {
@@ -18,6 +19,8 @@ namespace NetSwag.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+      
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
